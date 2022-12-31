2dcc547e36d42895ac6cc9980e3815fa1a4f3f10
John Park <yuntan0@gmail.com>
2022-12-31 09:46:40 +0000

    고객 코드 추가

--- hana_amt_credit/hana_amt_credit/doctype/credit_check/credit_check.py
+++ hana_amt_credit/hana_amt_credit/doctype/credit_check/credit_check.py
@@ -3,6 +3,7 @@
 
 # import frappe
 import re
+import frappe
 from frappe.model.document import Document
 
 class CreditCheck(Document):
@@ -10,5 +11,29 @@ class CreditCheck(Document):
 		self.country_code = self.country_code.upper()
 		self.bzno = re.sub("\-", "",  self.bzno)
 		self.name =  self.country_code.upper() + "-" + re.sub("\-", "",  self.bzno)
+	def after_insert(self):
+		# print(self.customer)
+		if self.customer:
+			frappe.db.set_value("Customer",self.customer,"credit_check",self.name)
+			# customer_doc = frappe.get_doc("Customer",self.customer)
+			# customer_doc.db_set('credit_check', self.name)
+			# customer_doc.save()
+		
+
+@frappe.whitelist()
+def make_credit_rate(doctype, docname):
+	doc = frappe.new_doc("Credit Check")
+	customer_doc = frappe.get_doc(doctype,docname)
+	doc.country = customer_doc.country
+	doc.country_code = customer_doc.country_code
+	doc.bzno = customer_doc.tax_id
+	doc.bzno = customer_doc.tax_id
+	doc.enp_nm = customer_doc.customer_name
+	doc.customer = docname
+	# doc.set(frappe.scrub(doctype), docname)
+	# doc.selling = 1 if doctype == "Customer" else 0
+	# doc.buying = 1 if doctype == "Supplier" else 0
+
+	return doc
 
 
